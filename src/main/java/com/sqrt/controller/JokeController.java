46eb2720929dd1ff0c31ca62bbe7a4576beb8423
Stefan Wilke <stefan.wilke@asideas.de>
2019-11-12 18:07:38 +0100

    new jokes

--- src/main/java/com/sqrt/controller/JokeController.java
+++ src/main/java/com/sqrt/controller/JokeController.java
@@ -19,18 +19,25 @@ public final class JokeController extends HttpServlet {
 		"My boss told me to have a good day .. so I went home.",
 		"If I got 50 cents for every failed math exam, I'd have $6.30 now.",
 		"I called the hospital but the line was dead.",
-		"I heard women love a man in uniform. Can't wait to start working at McDonalds.",
 		"If we shouldn't eat at night, why do they put a light in the fridge?",
 		"Chocolate is the best investment. You buy 100g – you gain 2kg!",
-		"I and Bill Gates have a combined fortune of approximately 80 billion dollars.",
+		"Bill Gates and I have a combined fortune of approximately 80 billion dollars.",
 		"You know you're ugly when you get handed the camera every time they make a group photo.",
-		"I hate people who take drugs. DEA is the worst.",
 		"My dog used to chase people on a bike a lot. It got so bad, finally I had to take his bike away.",
 		"A woman walks into a library and asked if they had any books about paranoia. The librarian answered they were right behind her.",
 		"I know a lot of jokes about unemployed people but none of them work.",
 		"I just wrote a book on reverse psychology. Do *not* read it!",
 		"Alcohol! Because no great story started with someone eating a salad.",
-		"Always remember you're unique, just like everyone else."
+		"Always remember you're unique, just like everyone else.",
+		"So what if I don't know what 'Armageddon' means? It's not the end of the world.",
+		"A lot of people cry when they cut onions. The trick is not to form an emotional bond.",
+		"I dressed my dog up as a cat for Halloween. Now he won't come when I call him.",
+		"Smile and the world smiles with you. Fart and the world suddenly stops smiling.",
+		"I have a parrot and it talks. Sadly it did not say it was hungry, so it died.",
+		"I asked God for a bike, but I know God doesn't work that way. So I stole a bike and asked for forgiveness.",
+		"Light travels faster than sound. This is why some people appear bright until you hear them speak.",
+		"What has four legs and an arm? A happy pit bull.",
+		"I thought I wanted a career, turns out I just wanted paychecks."
 	);
 
 	private static final Random random = new Random();
